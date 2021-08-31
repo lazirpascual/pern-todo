85a108de18a21bcc6178094b6dc3cac82cf5cd10
Lazir Pascual <lazirpascual@gmail.com>
2021-08-31 16:15:27 -0400

    remove client/pool connection in routes

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,8 +5,7 @@ const pool = require("../db");
 todosRouter.post("/", async (req, res) => {
   try {
     const { description } = req.body;
-    const client = await pool.connect();
-    const newTodo = await client.query(
+    const newTodo = await pool.query(
       "INSERT INTO todo (description) VALUES($1) RETURNING *",
       [description]
     );
@@ -19,8 +18,7 @@ todosRouter.post("/", async (req, res) => {
 // get all todos
 todosRouter.get("/", async (req, res) => {
   try {
-    const client = await pool.connect();
-    const allTodos = await client.query("SELECT * FROM todo");
+    const allTodos = await pool.query("SELECT * FROM todo");
     res.json(allTodos.rows);
   } catch (error) {
     console.log(error.message);
@@ -31,8 +29,7 @@ todosRouter.get("/", async (req, res) => {
 todosRouter.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const client = await pool.connect();
-    const todo = await client.query("SELECT * FROM todo WHERE todo_id = $1", [
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
       id,
     ]);
     res.status(200).json(todo.rows[0]);
@@ -46,8 +43,7 @@ todosRouter.put("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { description } = req.body;
-    const client = await pool.connect();
-    const updatedTodo = await client.query(
+    const updatedTodo = await pool.query(
       "UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *",
       [description, id]
     );
@@ -62,8 +58,7 @@ todosRouter.put("/:id", async (req, res) => {
 todosRouter.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const client = await pool.connect();
-    await client.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
     res.status(200).json("Todo was deleted!");
   } catch (error) {
     console.log(error.message);
